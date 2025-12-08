5ea0efbd862cc31402a0fc3765ea1e9b1747618f
Samuel D. Monteiro <samuel.dvmonteiro@gmail.com>
2025-12-08 16:03:07 -0300

    template dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,40 @@
-import LogoutButton from "@/src/components/logout-button";
-import { getUser } from "@/src/lib/dal"
+import { AppSidebar } from '@/src/components/app-sidebar'
+import { ChartAreaInteractive } from '@/src/components/chart-area-interactive'
+import { DataTable } from '@/src/components/data-table'
+import { SectionCards } from '@/src/components/section-cards'
+import { SiteHeader } from '@/src/components/site-header'
+import {
+  SidebarInset,
+  SidebarProvider,
+} from '@/src/components/ui/sidebar'
 
-const DashboardPage = async () => {
+import data from "./data.json"
 
-  const user = await getUser();
-
+export default function Page() {
   return (
-    <>
-      <div>{user?.name}</div>
-      <LogoutButton />
-    </>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              <DataTable data={data} />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
-
-export default DashboardPage
